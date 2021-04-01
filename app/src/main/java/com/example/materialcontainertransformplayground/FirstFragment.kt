aaf07813ce8355e15eb8e71a8b6172e8136ab612
Miguel Kano <miguel.g.kano@gmail.com>
2021-04-01 14:35:43 -0700

    update

--- app/src/main/java/com/example/materialcontainertransformplayground/FirstFragment.kt
+++ app/src/main/java/com/example/materialcontainertransformplayground/FirstFragment.kt
@@ -27,11 +27,6 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            /*(requireActivity() as MainActivity).run {
-                pop()
-                navigate(SecondFragment())
-            }*/
-
             val f = MyBottomSheetDialog()
             f.show(childFragmentManager, "fff")
         }
